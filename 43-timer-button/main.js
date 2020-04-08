21b7557dc23a2a6366ab43279bdf8ea1d6049ddb
秦继贤 <1653764529@qq.com>
2020-04-08 15:46:30 +0800

    timer-button2

--- 43-timer-button/main.js
+++ 43-timer-button/main.js
@@ -1,34 +1,42 @@
-define(['jquery'],function($){
-    function TimerButton(){
-        var $btn = $('<input class="timer-button" type="button" disabled>'),
-        cfg = {
-            container:'body',
-            num:6,
-            title:'同意',
-            onClick:null
-        },
-        num,
-        timer;
-
-        this.show = function(conf){
-            $(cfg.container).append($btn);
-            $.extend(cfg,conf);
-            num = cfg.num;
-
-            $btn.val(cfg.title+'('+cfg.num+'s)');
-
-            timer = setInterval(function(){
-                num--;
-                if(num === 0){
-                    clearInterval(timer);
-                    $btn.val(cfg.title);
-                    $btn.removeAttr('disabled');
-                }else{
-                    $btn.val(cfg.title + '(' + num + 's)');
-                }
-            },1000);
-            $btn.click(cfg.onClick);
+requirejs.config({
+    'paths':'https://cdn.bootcss.com/jquery/3.4.1/jquery.js'
+  })
+  
+  require(['jquery']),function($){
+    $(function(){
+      var $btnAdd = $('#add'),
+          $enabled = $('#enabled'),
+          $unabled = $('#unabled'),
+          $num = $('#number'),
+          $title = $('#text'),
+          $eventCode = $('textarea'),
+          opt={};
+      $enabled.click(function(){
+        opt.disable=true;
+      })
+      $unabled.click(function(){
+        opt.disable=false;
+      })
+      $num.change(function(){
+        opt.num=Number($num.val());
+      })
+      $title.change(function(){
+        if($title.val() !== ""){
+          opt.title=$title.val();
+        }else{
+          opt.title="同意";
         }
-    }
-    return TimerButton;
-})
+      })
+      // $timerButton.show(opt);
+      $btnAdd.click(function(){
+        opt.container="#main";
+        opt.onClick=function(){
+          eval($eventCode.html());
+        }
+        require(['index.js']),function(){
+          $timerButton.show(opt);
+        }
+  
+      })
+    })
+  }
